e87e387f3dbf2cf5b5fd61c67e27f8eff897a206
Fadi Hanna Al-Kass <f_alkass@zgps.live>
2020-01-11 22:28:19 -0600

    Instructions to handle other platforms

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup
 from setuptools.command.install import install
-import os
+import sys, os, stat
+from shutil import copyfile
+
+__PROG_NAME__ = 'zdeploy'
 
 class Installer(install):
     def run(self):
-        print(os.popen('cp zdeploy /usr/bin/').read())
+        print('Installing %s' % __PROG_NAME__)
+        platform = sys.platform.lower()
+        if 'linux' in platform:
+                dest = '/usr/bin/%s' % __PROG_NAME__
+                copyfile(__PROG_NAME__, dest)
+                os.chmod(dest, os.stat(dest).st_mode | stat.S_IEXEC | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        else:
+                print('Please manually copy %s to a local directory and add its path to your system environment variables' % __PROG_NAME__)
 
 setup(
-        name='zdeploy',
-        url='https://github.com/ziggurattech/zdeploy',
+        name=__PROG_NAME__,
+        url='https://github.com/ziggurattech/%s' % __PROG_NAME__,
         author='Fadi Hanna Al-Kass',
         description='General-purpose host deployment utility',
         long_description=open('README.md').read(),
         cmdclass={'install': Installer},
-        install_requires=[
-            'paramiko',
-            'requests',
-            'python-dotenv',
-            'scp',
-        ],
+        install_requires=open('requirements.txt').read().split('\n'),
 )
